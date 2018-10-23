62f6e75563936beeb98e7c8e0c496c3d3ac73c2f
vdc1703 <vdc1703@gmail.com>
2018-10-23 13:10:58 -0400

    HW5

---
+++ HW5/inc/footer.php
@@ -0,0 +1,16 @@
+<!--Display footer-->
+        <div id="footer">   
+                <p>Under Development &copy; 2018 Chuong Vu - 91.61 Internet Web System I</p>
+                <p>This page is valid 
+                    <a href="http://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fvdc1703.github.io%2FGUI-1%2Findex.html+&profile=css3&usermedium=all&warning=1&vextwarning=&lang=en"><img style="border:0;width:4%" src="img/valid-css.png" alt="Valid CSS!"></a> 
+                    and
+                    <a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fvdc1703.github.io%2FGUI-1%2Findex.html"><img style="border:0;width:4%" src="img/html5-courses.png" alt="Valid HTML5"></a>
+                    per W3C
+                </p>
+                <br>
+                <br>
+        </div>
+            <!-- <div class="clear"></div> -->
+    </body>
+
+</html>
---
+++ HW5/inc/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<!--
+    Name: Chuong Vu
+    Email: Chuong_Vu@student.uml.edu
+    Major: Computer Science
+    School: University of Massachusetts Lowell
+    Course: 91.61 GUI Programming I
+    Date created: 9/8/2016
+    Update Date: 10/4/2016
+    Link: http://weblab.cs.uml.edu/~cvu/index.html
+    css: http://weblab.cs.uml.edu/~cvu/css/style.css
+
+    Description: Assignment 4: Styling Your First Web Page With CSS
+    This is about page, this descript about myself and my study plan in the future
+    Resouces: http://www.w3schools.com/html/default.asp 
+-->
+
+<html lang="en-US">
+    <head>
+            <title>UML - Chuong Vu</title>
+            <meta charset="UTF-8">
+            <!-- stylesheets -->
+            <link type="text/css" rel="stylesheet" href="css/style.css">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <!-- Body tag -->
+    <body>
+        <!-- Main Div -->
+        <div id='header' class="container-fluid"> 
+            <!-- header container -->
+            <div class='text-header'>
+                
+                <!-- not in use now at this time since I dont have a good profile picture -->
+                <!-- <div class=''> -->
+                    <!-- <img class="img-profile" src ="img/profile.png" alt="Profile Picture"> -->
+                <!-- </div> -->
+            </div>
+        </div>
+
+        <!-- About Me -->
+        <div class="container-fluid pad-top-50 about">
+            <div class="text-center">
+                <h1>About Website</h1>
+            </div>
+            <div class="space">
+                <p>You will see many beautiful pictures that from our website</p>
+                <p>Click any link below from our user to get to their albums.</p>
+            </div>
+        </div>
