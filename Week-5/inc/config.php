0bfa946fd0e900c2dc1b33e35069a00bb4ce5d0f
vdc1703 <vdc1703@gmail.com>
2018-11-06 12:03:46 -0500

    up

--- Week-5/inc/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Configuration
-$dbHost = "localhost";	//Database Location
-$dbuser= "root";	//Database User
-$dbpwd= "root";	//Database User Password
-$dbname = "test";	//Account Database Name
-?>
